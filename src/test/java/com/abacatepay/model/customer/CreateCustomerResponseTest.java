0294b1891ffb4aa02e0a12a5f10df9927e8c5675
Joel Filho <joelfilho02812@gmail.com>
2024-12-11 11:46:42 -0300

    test: customers models

---
+++ src/test/java/com/abacatepay/model/customer/CreateCustomerResponseTest.java
@@ -0,0 +1,24 @@
+package com.abacatepay.model.customer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class CreateCustomerResponseTest {
+    @Test
+    void testErrorConstructor() {
+        CreateCustomerResponse response = new CreateCustomerResponse("Error");
+        assertEquals("Error occurred", response.getError(), "Error field must be set correctly");
+    }
+
+    @Test
+    void testGettersAndSetters() {
+        CreateCustomerResponse response = new CreateCustomerResponse();
+        response.setError("Error occurred");
+        response.setData(null);
+
+        assertEquals("Error occurred", response.getError(), "Error field must be set correctly");
+        assertNull(response.getData(), "Data field must be set correctly");
+    }
+}
