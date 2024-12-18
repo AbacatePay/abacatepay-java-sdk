61d04ee47677121257f93050c2a7a3ccb8514651
Erickson Dias <103853894+EricksonLOOP@users.noreply.github.com>
2024-12-18 15:44:41 -0300

    Delete TesteFiles
    Deletando os arquivos de testes para uma escritura de testes mais centralizados.

--- src/test/java/com/abacatepay/AbacatePayTest.java
+++ src/test/java/com/abacatepay/AbacatePayTest.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 class AbacatePayTest {
     public static void main(String[] args){
-        SpringApplication.run(AbacatePaySDK.class, args);
+        SpringApplication.run(AbacatePayTest.class, args);
     }
 }
