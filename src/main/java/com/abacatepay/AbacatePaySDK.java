bb3b02313beb01bc0ec39e201a7c03e502e182c8
Erickson Dias <103853894+EricksonLOOP@users.noreply.github.com>
2024-12-17 21:32:51 -0300

    Add/SpringBoot Framework
    Adicionando SpringBoot Framework para melhorar o mantimento das features

---
+++ src/main/java/com/abacatepay/AbacatePaySDK.java
@@ -0,0 +1,9 @@
+package com.abacatepay;
+
+import org.springframework.boot.SpringApplication;
+
+public class AbacatePaySDK {
+    public static void main(String[] args){
+        SpringApplication.run(AbacatePaySDK.class, args);
+    }
+}
