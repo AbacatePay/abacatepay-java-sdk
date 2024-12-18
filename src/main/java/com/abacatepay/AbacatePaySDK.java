90b57f8a5600a8a2562a3e0a7643f865c1023c22
Erickson Dias <103853894+EricksonLOOP@users.noreply.github.com>
2024-12-17 21:34:43 -0300

    HotFix/SpringBoot & Feing Annotation
    Adicionando anotação de inicialização do SpringBoot e Feing

--- src/main/java/com/abacatepay/AbacatePaySDK.java
+++ src/main/java/com/abacatepay/AbacatePaySDK.java
@@ -1,7 +1,11 @@
 package com.abacatepay;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@SpringBootApplication
+@EnableFeignClients
 public class AbacatePaySDK {
     public static void main(String[] args){
         SpringApplication.run(AbacatePaySDK.class, args);
