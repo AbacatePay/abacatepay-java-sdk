5a2ad222ab989ed653545bc4d3519dc983be2cf6
Erickson Dias <103853894+EricksonLOOP@users.noreply.github.com>
2024-12-25 13:53:04 -0300

    Add @Component at config

--- src/main/java/com/abacatepay/Utils/Config/AbacatePayConfig.java
+++ src/main/java/com/abacatepay/Utils/Config/AbacatePayConfig.java
@@ -3,10 +3,12 @@ package com.abacatepay.Utils.Config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Data
 @ConfigurationProperties(prefix = "abacatepay")
 @Configuration
+@Component
 public class AbacatePayConfig {
     private String baseUrl;
     private String apiKey;
